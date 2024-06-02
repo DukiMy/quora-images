af529be6a7d186996e49a826deda3a7ebac51fff
Durim Miziraj <durim.miziraj@gmail.com>
2024-06-02 18:07:07 +0200

    Updated the image and cleaned the code.

--- CanvasBuilder.cs
+++ CanvasBuilder.cs
@@ -3,51 +3,48 @@ using SixLabors.ImageSharp.PixelFormats;
 using SixLabors.ImageSharp.Processing;
 using SixLabors.ImageSharp.Drawing.Processing;
 using SixLabors.ImageSharp.Metadata;
-using System.Runtime.CompilerServices;
 
-namespace quora_images
-{
+namespace quora_images;
 public class CanvasBuilder
+{
+    private int width;
+    private int height;
+    private int ppi;
+    private Color bgColor;
+
+    public CanvasBuilder setWidth(int width)
     {
-        private int width;
-        private int height;
-        private int ppi;
-        private Color bgColor;
+        this.width = width;
+        return this;
+    }
 
-        public CanvasBuilder setWidth(int width)
-        {
-            this.width = width;
-            return this;
-        }
+    public CanvasBuilder setHeight(int height)
+    {
+        this.height = height;
+        return this;
+    }
 
-        public CanvasBuilder setHeight(int height)
-        {
-            this.height = height;
-            return this;
-        }
+    public CanvasBuilder setPpi(int ppi)
+    {
+        this.ppi = ppi;
+        return this;
+    }
 
-        public CanvasBuilder setPpi(int ppi)
-        {
-            this.ppi = ppi;
-            return this;
-        }
+    public CanvasBuilder setBgColor(Color bgColor)
+    {
+        this.bgColor = bgColor;
+        return this;
+    }
 
-        public CanvasBuilder setBgColor(Color bgColor)
-        {
-            this.bgColor = bgColor;
-            return this;
-        }
+    public Image<Rgba32> buildCanvas()
+    {
+        var image = new Image<Rgba32>(width, height);
+        image.Metadata.ResolutionUnits = PixelResolutionUnit.PixelsPerInch;
+        image.Metadata.HorizontalResolution = ppi;
+        image.Metadata.VerticalResolution = ppi;
 
-        public Image<Rgba32> buildCanvas()
-        {
-            var image = new Image<Rgba32>(width, height);
-            image.Metadata.ResolutionUnits = PixelResolutionUnit.PixelsPerInch;
-            image.Metadata.HorizontalResolution = ppi;
-            image.Metadata.VerticalResolution = ppi;
+        image.Mutate(ctx => ctx.Fill(bgColor));
 
-            image.Mutate(ctx => ctx.Fill(bgColor));
-
-            return image;
-        }
+        return image;
     }
 }
--- Program.cs
+++ Program.cs
@@ -2,61 +2,94 @@
 using SixLabors.ImageSharp.PixelFormats;
 using SixLabors.ImageSharp.Processing;
 using SixLabors.ImageSharp.Drawing.Processing;
-using SixLabors.ImageSharp.Metadata;
-using System.Runtime.CompilerServices;
+using SixLabors.ImageSharp.Drawing;
 
-namespace quora_images
+namespace quora_images;
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        private const string path = "output.png"; 
-        private static Color snekColor = Color.Red;
-        private static SolidBrush snek = Brushes.Solid(snekColor);
-        private static int snekWidth = 50;
-        private static int yPosition;
-        private static bool movingRight;
-        private static int xStart;
-        private static int xEnd;
+        // The greatest height possible for an image on quora.com
+        int width = 1;
+        int height = 1000000;
+        int ppi = 1;
 
-        static void Main(string[] args)
+        using (Image<Rgba32> image = new CanvasBuilder()
+            .setWidth(width)
+            .setHeight(height)
+            .setPpi(ppi)
+            .setBgColor(Color.FromRgb(38, 38, 38))
+            .buildCanvas())
         {
-            using (Image<Rgba32> image = new CanvasBuilder()
-                .setWidth(580)
-                .setHeight(154273)
-                .setPpi(72)
-                .setBgColor(Color.BlanchedAlmond)
-                .buildCanvas())
+
+        // bool drawRed = true;
+        // for (int i = 0; i < height; i++)
+        // {
+        //     if (i % 100 == 0)
+        //     {
+        //         drawRed = !drawRed; // Toggle the drawing state every 50 pixels
+        //     }
+
+        //     if (drawRed)
+        //     {
+        //         // Draw a dot at the current pixel height
+        //         image.Mutate(x => x.Fill(Color.FromRgba(0, 0, 0, 0), new EllipsePolygon(0, i, 0.5f)));
+        //     }
+        // }
+
+            // Save the image
+            image.Save(height + "_" + width + "_" + ppi + ".png");
+            Console.WriteLine("Image has been saved successfully.");
+        }
+    }
+
+    public static void StripedImage (int width, int height, int ppi) {
+        
+        Color snekColor = Color.Red;
+        SolidBrush snek = Brushes.Solid(snekColor);
+        int snekWidth = 50;
+        int yPosition;
+        bool movingRight;
+        int xStart;
+        int xEnd;
+
+
+        using (Image<Rgba32> image = new CanvasBuilder()
+            .setWidth(width)
+            .setHeight(height)
+            .setPpi(ppi)
+            .setBgColor(Color.BlanchedAlmond)
+            .buildCanvas())
+        {
+            // Prepare to draw the snek
+            yPosition = 0;
+            movingRight = true;
+
+            while (yPosition < image.Height)
             {
-                // Prepare to draw the snek
-                yPosition = 0;
-                movingRight = true;
+                xStart = movingRight ? 0 : image.Width;
+                xEnd = movingRight ? image.Width : 0;
 
-                while (yPosition < image.Height)
+                // Draw the full width part (Section 1)
+                image.Mutate(x => x.FillPolygon(snek,
+                new PointF[]
                 {
-                    xStart = movingRight ? 0 : image.Width;
-                    xEnd = movingRight ? image.Width : 0;
+                    new PointF(xStart, yPosition),
+                    new PointF(xEnd, yPosition),
+                    new PointF(xEnd, yPosition + snekWidth),
+                    new PointF(xStart, yPosition + snekWidth)
+                }));
 
-                    // Draw the full width part (Section 1)
-                    image.Mutate(x => x.FillPolygon(snek,
-                    new PointF[]
-                    {
-                        new PointF(xStart, yPosition),
-                        new PointF(xEnd, yPosition),
-                        new PointF(xEnd, yPosition + snekWidth),
-                        new PointF(xStart, yPosition + snekWidth)
-                    }));
+                // Increment y by the snek width to start the next section
+                yPosition += snekWidth * 2;
 
-                    // Increment y by the snek width to start the next section
-                    yPosition += snekWidth * 2;
-
-                    // Reverse the direction for the next draw
-                    movingRight = !movingRight;
-                }
-
-                // Save the image
-                image.Save(path);
-                Console.WriteLine("Image has been saved successfully.");
+                // Reverse the direction for the next draw
+                movingRight = !movingRight;
             }
+
+            // Save the image
+            image.Save(height + "_" + width + "_" + ppi + ".png");
+            Console.WriteLine("Image has been saved successfully.");
         }
     }
 }
